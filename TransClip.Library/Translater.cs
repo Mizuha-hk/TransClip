9a00f394ebb5fcd7a8fc9d31ecbc8a67c2093105
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-08-08 22:32:02 +0900

    [add]:Translater Language

---
+++ TransClip.Library/Translater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeepL;
+
+namespace TransClip.Library
+{
+    public static class Translater
+    {
+        
+        public struct Language
+        {
+            public const string EN = "EN";
+            public const string JP = "JA";
+        }
+
+        
+    }
+}

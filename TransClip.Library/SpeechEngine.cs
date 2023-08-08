9358c7c8b1d33e7abf4ee7ff57f545f2ddfa3705
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-08-08 14:50:57 +0900

    [add]:speech library

---
+++ TransClip.Library/SpeechEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace TransClip.Library
+{
+    public class SpeechEngine
+    {
+        public void Say(string text)
+        {
+            var synthesizer = new SpeechSynthesizer();
+
+            synthesizer.SetOutputToDefaultAudioDevice();
+
+            synthesizer.SpeakAsync(text);
+        }
+    }
+}

8a06f4dee4303f08265f39601b732d3155ead884
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-08-13 05:20:16 +0900

    [add]: Translator

--- TransClip.Library/Translator.cs
+++ TransClip.Library/Translator.cs
@@ -1,21 +1,72 @@
-﻿using System;
+﻿using System; 
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DeepL;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace TransClip.Library
 {
-    public static class Translator
+    public class Translator
     {
-        
+        private bool IsPro { get; set; }
+        private string AuthKey { get; set; }
+
+        private const string freeUri = "https://api-free.deepl.com/v2/translate";
+        private const string proUri = "https://api.deepl.com/v2/translate";
+
         public struct Language
         {
             public const string EN = "EN";
             public const string JP = "JA";
         }
-
         
+        public Translator(bool isPro ,string authKey)
+        {
+            this.IsPro = isPro;
+            this.AuthKey = authKey;
+        }
+
+        public void ChangeDeepLSettings(bool isPro,  string authKey)
+        {
+            this.IsPro = isPro;
+            this.AuthKey = authKey;
+        }
+
+        public async Task<string> TranslateAsync(string text, string lang)
+        {
+            var uri = IsPro ? proUri : freeUri;
+
+            using (var http = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), uri))
+                {
+                    var contentList = new List<string>
+                    {
+                        "auth_key=" + AuthKey,
+                        "text=" + text,
+                        "target_lang=" + lang
+                    };
+
+                    request.Content = new StringContent(string.Join("&", contentList));
+                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+
+                    var response = await http.SendAsync(request);
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    var jObject = JsonSerializer.Deserialize<translations>(responseString);
+                    return jObject.text;
+                }
+            }
+        }
+
+        private class translations
+        {
+            public string detected_source_language { get; set; }
+            public string text { get; set; }
+        }
     }
 }
---
+++ TransClip.Test/LibTest.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransClip.Library;
+
+namespace TransClip.Test
+{
+    [TestClass]
+    public class LibTest
+    {
+        [TestMethod]
+        public async Task TranslatorTest()
+        {
+            var translator = new Translator(false, "[InputAuthKey]");
+            var text = await translator.TranslateAsync("Hello World", Translator.Language.EN);
+
+            Console.WriteLine(text);
+        }
+    }
+}

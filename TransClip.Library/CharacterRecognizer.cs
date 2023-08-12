c026e0ec61c116984a7ab7000dd43dc3aea7b445
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-08-13 07:14:42 +0900

    [delete]: unused using directive

--- TransClip.Library/CharacterRecognizer.cs
+++ TransClip.Library/CharacterRecognizer.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
 using Windows.Media.Ocr;
-using Windows.UI.Xaml.Shapes;
 
 namespace TransClip.Library
 {
--- TransClip.Library/Translator.cs
+++ TransClip.Library/Translator.cs
@@ -1,12 +1,8 @@
-﻿using System; 
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace TransClip.Library
 {

5b072641bf28f3ead3edecca7ec3075e4a6d69a5
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-08-21 10:47:23 +0900

    [add]: setting page content

--- TransClip.UI/Pages/SettingPage.xaml.cs
+++ TransClip.UI/Pages/SettingPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,11 @@ namespace TransClip.UI.Pages
         public SettingPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void SourceCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri("https://github.com/Mizuha-hk/TransClip"));
         }
     }
 }
